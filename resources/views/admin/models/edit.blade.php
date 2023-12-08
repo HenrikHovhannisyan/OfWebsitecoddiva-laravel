727b0852f0cb6fba3207ebacfe96347359408586
HenrikHovhannisyan <henrik.hovhannisyan.91@gmail.com>
2023-12-08 11:44:54 +0400

    Fixed bug

--- resources/views/admin/models/edit.blade.php
+++ resources/views/admin/models/edit.blade.php
@@ -5,7 +5,7 @@
         <div class="row">
             <div class="col-lg-12 margin-tb">
                 <div class="pull-left">
-                    <h2>Model Info</h2>
+                    <h2>Edit Model</h2>
                 </div>
                 <div class="pull-right">
                     <a class="btn btn-primary" href="{{ route('models.index') }}"> Back</a>
