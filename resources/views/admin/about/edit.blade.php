66231975a48246627e981d7aceb78c7b27ef9170
HenrikHovhannisyan <henrik.hovhannisyan.91@gmail.com>
2024-02-02 11:26:34 +0400

    Fixed bug

--- resources/views/admin/about/edit.blade.php
+++ resources/views/admin/about/edit.blade.php
@@ -8,7 +8,7 @@
                     <h2>Edit About</h2>
                 </div>
                 <div class="pull-right">
-                    <a class="btn btn-primary" href="{{ route('blogs.index') }}"> Back</a>
+                    <a class="btn btn-primary" href="{{ route('about.index') }}"> Back</a>
                 </div>
             </div>
         </div>
